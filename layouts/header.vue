0f8cc20aab3f410246e6e2ab20a942e5143ef57a
paulozabala <balticst3ch@gmail.com>
2025-03-26 15:30:35 -0500

    feat(header layout): create navigation bar
    create navigation bar for medicoop webpage (responsiveness included)

---
+++ layouts/header.vue
@@ -0,0 +1,69 @@
+<template fluid>
+  <div id="MainPage" class="flex flex-row justify-center">
+    <div id="topMenu" >
+      <div class="boxoutofwhitebox">
+        <div id="logobox" >
+          <img src="../public/logonly.png" alt="logo" class="logo" />
+        </div>
+      </div>
+      <div>Inicio</div>
+      <div>Conócenos</div>
+      <div>Blog</div>
+      <div>Servicios</div>
+      <div>Contácto</div>
+      <div>Acceso Usuarios</div>
+    </div>
+  </div>
+</template>
+<style>
+#MainPage {
+  background-color: white;
+  width: 100%;
+  height: 100vh;
+}
+
+#topMenu {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: end;
+  gap: 8px;
+  height: 60px;
+  background-color: #2DCD7F;
+  min-width:100%;
+}
+
+#topMenu div{
+  display: flex;
+  justify-content: center ;
+  align-items: center;
+  width: 14.28% !important;
+  /*font-size: 26px;*/
+  font-weight:bolder;
+}
+
+.boxoutofwhitebox{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+}
+
+.logo {
+  width: 60px;
+  height: 60px;
+}
+
+#logobox{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  border-radius:500px 0px 500px 0px;
+  height: 80%;
+  min-width: 80%;
+  max-width: 90%;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <NuxtLayout name="header" />
+</template>
+
+<script setup lang="ts">
+
+import { useRoute } from "vue-router";
+
+const loginw = ref<boolean>(false)
+const successLogin = ref<boolean>(false)
+const inValidLogin = ref<boolean>(false)
+
+//routing
+const router = useRouter()
+const route = useRoute()
+
+
+onMounted(() => {
+    loginw.value = true;
+})
+
+interface logformStructure {
+    nombre: string,
+    password: string,
+    perfil: string
+}
+
+const logform = reactive<logformStructure>({
+    nombre: "",
+    password: "",
+    perfil: "",
+})
+
+
+function usernotfoundmsg() {
+    alert("mensjae: no encontrado")
+    //if (loginErrMsgRef.value) loginErrMsgRef.value.focus();
+}
+</script>
+
+<style>
+
+div>input {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: medium;
+    border-radius: 3px;
+    margin: 0px 15px;
+    padding: 5px;
+}
+
+button {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: auto !important;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: large;
+    font-weight: bold;
+    color: white;
+    border-width: 1px;
+    margin: 0px 45px;
+}
+</style>
