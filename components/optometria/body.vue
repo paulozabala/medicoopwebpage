d39a229531da13dcebc256e1c6f86e7206d5681e
paulozabala <balticst3ch@gmail.com>
2025-04-24 15:32:08 -0500

    Add optometry page with header, body, and footer components
    - Created a new body component for the optometry section, including an image and descriptive text about optometry services.
    - Developed a new optometry page that integrates the header, body, and footer components for a complete layout.
    - Styled the components to ensure a cohesive design and user-friendly experience.

--- components/optometria/body.vue
+++ components/optometria/body.vue
@@ -2,21 +2,20 @@
     <div class="modalmenu flex flex-col mb-4 relative ">
         <div class="flex flex-col justify-center items-center px-0 z-2 gap-4">
             <div class="flex flex-row justify-center bg-blue-100 shadow-2xl w-full px-2 ">
-                <img class="rounded-b-3xl w-full lg:w-180 mb-2 lg:h-96" src="../../assets/nutricion.jpg" alt="">
+                <img class="rounded-b-3xl w-full lg:w-180 mb-2 lg:h-96" src="../../assets/optometria.jpg" alt="">
             </div>
             <div class="px-12">
 
                 <div class="text-blue-100 font-bold shadow-2xl text-center sm:text-start fill-old-neutral-600 text-2xl">
-                    Nutrición y Dietética</div>
+                    Optometría</div>
                 <div class="flex flex-col sm:flex-row gap-4 justify-around w-full items-center">
                     <div class="flex flex-col justify-center items-start text-xl text-blue-100 gap-2">
                         <p class="text-justify">
-                            <span class="pl-4">La nutrición y dietética</span>son pilares fundamentales para una vida saludable.
-                            En Medicoop IPS, ofrecemos asesoramiento personalizado para ayudarte a
-                            alcanzar tus objetivos de bienestar, combinando ciencia y experiencia para diseñar
-                            planes alimenticios equilibrados que potencien tu energía, fortalezcan tu sistema
-                            inmunológico y mejoren tu calidad de vida. ¡Descubre el poder de una vida saludable
-                            con nosotros!
+                            <span class="pl-4">La optometría</span> es una disciplina de la salud que mide, diagnostica, 
+                            previene y corrige defectos o deficiencias de la visión. En Medicoop IPS realizamos pruebas 
+                            de visión para detectar problemas como miopía, hipermetropía, astigmatismo y otras anomalías.
+                            Escribenos y te ayudaremos a identificar problemas de visión que pueden estar afectando tu 
+                            desempeño en tus estudios o en el trabajo. 
                         </p>
                     </div>
                 </div>
