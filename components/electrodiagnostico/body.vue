cd83642081c540c6a7e939e57f317bfe75ef6946
paulozabala <balticst3ch@gmail.com>
2025-04-24 16:08:44 -0500

    feat: Add page and component for electrodiagnostic procedures, including content and styles

---
+++ components/electrodiagnostico/body.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="modalmenu flex flex-col mb-4 relative ">
+        <div class="flex flex-col justify-center items-center px-0 z-2 gap-4">
+            <div class="flex flex-row justify-center bg-blue-100 shadow-2xl w-full px-2 ">
+                <img class="rounded-b-3xl w-full lg:w-180 mb-2 lg:h-96" src="../../assets/electrodiagnostico.jpg"
+                    alt="">
+            </div>
+            <div class="px-12">
+                <div
+                    class="text-blue-100 font-bold shadow-2xl text-center sm:text-start fill-old-neutral-600 text-2xl mb-3">
+                    Procedimientos de electrodiagnóstico</div>
+                <div class="flex flex-col sm:flex-row gap-4 justify-around w-full items-center">
+                    <div class="flex flex-col justify-center text-xl text-blue-100 gap-2">
+                        <p class="text-justify">
+                            <span class="pl-8">Es una disciplina</span> de la salud que mide, diagnostica,
+                            Los procedimientos de electrodiagnóstico son técnicas utilizadas para evaluar
+                            la función nerviosa y muscular. Estos procedimientos incluyen principalmente
+                            dos tipos de pruebas: estudios de conducción nerviosa (NCS) y electromiografía
+                            con aguja (EMG).
+                        </p>
+                        <p>
+                            <span class="pl-8 font-bold">Estudio de conducción nerviosa (NCS):</span> se colocan
+                            electrodos en la piel y se
+                            aplican pequeños pulsos eléctricos para estimular los nervios. Mide la velocidad
+                            y la fuerza de las señales nerviosas, lo que ayuda a detectar problemas como la
+                            neuropatía.
+                        </p>
+                        <p>
+                            <span class="pl-8 font-bold">Electromiografía con aguja (EMG):</span> se inserta una pequeña
+                            aguja en un músculo para
+                            registrar su actividad eléctrica en reposo y durante la contracción. Esta prueba
+                            se puede utilizar para diagnosticar enfermedades musculares y neuromusculares.
+
+                            En Medicoop IPS, contamos con:
+                        </p>
+                        <ul class="list-disc pl-10">
+                            <li>Electromiografía</li>
+                            <li>Potenciales Evocados</li>
+                            <li>Neuroconducción</li>
+                            <li>Onda F</li>
+                            <li>Reflejo H</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
